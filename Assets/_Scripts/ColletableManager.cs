6f18434f65ebc4e3cabc35c04805d54207103834
Yaiza <yaizacastillo@users.noreply.github.com>
2019-07-12 17:48:18 +0200

    Coleccionables 2.0

---
+++ Assets/_Scripts/ColletableManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColletableManager : MonoBehaviour
+{
+    public AudioSource m_audiosource;
+
+    public List<AudioClip> m_collectableSounds = new List<AudioClip>();
+
+    public void OnClicked(Button button)
+    {
+        foreach(AudioClip sound in m_collectableSounds)
+        {
+            if(sound.name == button.tag)
+            {
+                m_audiosource.PlayOneShot(sound);
+                return;
+            }
+        }
+    }
+}
